2aaa3c30216d66b5bc9ce8178f0aa8fa422a1251
Chavda Mahesh <chavdamahesh2772@gmail.com>
2023-09-02 12:30:34 +0530

    improve development expirence

--- watchFile.js
+++ watchFile.js
@@ -1,20 +1,39 @@
 export default [
-  'public/manifest.json',
-  'public/backgroundService/backgroundService.js',
-  'public/contentScript/contentScript.js',
+    'public/manifest.json',
+    'public/backgroundService/backgroundService.js',
+    'public/contentScript/contentScript.js',
 
-  // Popup
-  'public/popup/popup.html',
-  'public/popup/popup.js',
+    // Popup
+    'public/popup/popup.html',
+    'public/popup/popup.js',
 
-  // Devtools
-  'public/devtools/devtools.html',
-  'public/devtools/devtools.js',
-  //'public/devtools/sidebarPanel.html',
-  'public/devtools/sidebarPanel.js',
+    // Devtools
+    'public/devtools/devtools.html',
+    'public/devtools/devtools.js',
+    //'public/devtools/sidebarPanel.html',
+    'public/devtools/sidebarPanel.js',
 
-  // Devtools/Sandbox
-  'public/devtools/sandbox/sandbox.html',
-  'public/devtools/sandbox/sandbox.js',
-  'public/devtools/sandbox/styles/styles.css',
+    // Devtools/Sandbox
+    'public/devtools/sandbox/sandbox.html',
+    'public/devtools/sandbox/sandbox.js',
+    'public/devtools/sandbox/styles/styles.css',
+
+    // framework files    
+    'public/devtools/sandbox/app/common/CommonHelper.js',
+    'public/devtools/sandbox/app/common/Enums.js',
+    'public/devtools/sandbox/app/common/TempData.js',
+    'public/devtools/sandbox/app/controller/HierarchyGridController.js',
+    'public/devtools/sandbox/app/controller/JsonDataViewerController.js',
+    'public/devtools/sandbox/app/controller/JsonTreeViewController.js',
+    'public/devtools/sandbox/app/controller/ViewportController.js',
+    'public/devtools/sandbox/app/helperClass/ComponentLocator.js',
+    'public/devtools/sandbox/app/helperClass/HoverIn.js',
+    'public/devtools/sandbox/app/helperClass/HoverOut.js',
+    'public/devtools/sandbox/app/helperClass/NewComponentDetails.js',
+    'public/devtools/sandbox/app/helperClass/UpdateComponent.js',
+    'public/devtools/sandbox/app/view/AddConfig.js',
+    'public/devtools/sandbox/app/view/HierarchyGrid.js',
+    'public/devtools/sandbox/app/view/JsonDataViewer.js',
+    'public/devtools/sandbox/app/view/JsonTreeView.js',
+    'public/devtools/sandbox/app/view/Viewport.js',
 ];
