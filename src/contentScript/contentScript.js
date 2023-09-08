7daa9c1c1e2de14807f8c0a6dc7ccb752d33c28a
Chavda Mahesh <chavdamahesh2772@gmail.com>
2023-09-08 13:38:35 +0530

    Added content script

--- src/contentScript/contentScript.js
+++ src/contentScript/contentScript.js
@@ -1,11 +1,31 @@
-import src from './logo.png';
-import './content.css';
+console.log('Content Scripts Loaded!');
+// var port = chrome.runtime.connect();
+// var count = 1;
 
-const html = `
-<div class="crx">
-  <img src="${chrome.runtime.getURL(src)}" />
-</div>
-`;
+//receiving a message
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === 'checkForExtJS') {
+//     checkForExtJS();
+//     return;
+//   }
+//   if (message.msgType) {
+//     // const cmpId = setSelectedElement($0);
+//     sendResponse({ msgType: 'getCmpId', isExtJs: isExtJs });
+//   }
 
-const doc = new DOMParser().parseFromString(html, 'text/html');
-document.body.append(doc.body.firstElementChild);
+//   console.log('content script message recived !!');
+//   console.log('recived :-' + message + ',response = ' + document.title);
+//   count = count + 1;
+//   sendResponse({ title: document.title + ' ' + count });
+// });
+
+// function checkForExtJS() {
+//   // Check if the 'Ext' object exists in the global scope
+//   if (window.Ext && typeof window.Ext.versions === 'object') {
+//     // Ext JS is loaded, send a message to DevTools panel
+//     // chrome.runtime.sendMessage({ extVersion: window.Ext.versions.ext.version });
+//     return window.Ext.versions.ext.version;
+//   } else {
+//     return 'Not loaded';
+//   }
+// }
