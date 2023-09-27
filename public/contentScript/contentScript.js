cfa23a4658fcad2813ef08894ec710277e4ecaaa
Chavda Mahesh <chavdamahesh2772@gmail.com>
2023-09-27 20:55:49 +0530

    Popup to detect Ext Js version change and version incrment

--- public/contentScript/contentScript.js
+++ public/contentScript/contentScript.js
@@ -1,31 +1,12 @@
+let div = document.createElement('div');
+div.id = 'spyglass-ext-details';
+div.style.display = 'none';
+
+div.innerHTML = JSON.stringify({
+    isExtJs: !!window.Ext?.versions?.ext?.version,
+    extJsVersion: window.Ext?.versions?.ext?.version ?? ''
+});
+
+document.body.appendChild(div);
+
 console.log('Content Scripts Loaded!');
-// var port = chrome.runtime.connect();
-// var count = 1;
-
-//receiving a message
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.action === 'checkForExtJS') {
-//     checkForExtJS();
-//     return;
-//   }
-//   if (message.msgType) {
-//     // const cmpId = setSelectedElement($0);
-//     sendResponse({ msgType: 'getCmpId', isExtJs: isExtJs });
-//   }
-
-//   console.log('content script message recived !!');
-//   console.log('recived :-' + message + ',response = ' + document.title);
-//   count = count + 1;
-//   sendResponse({ title: document.title + ' ' + count });
-// });
-
-// function checkForExtJS() {
-//   // Check if the 'Ext' object exists in the global scope
-//   if (window.Ext && typeof window.Ext.versions === 'object') {
-//     // Ext JS is loaded, send a message to DevTools panel
-//     // chrome.runtime.sendMessage({ extVersion: window.Ext.versions.ext.version });
-//     return window.Ext.versions.ext.version;
-//   } else {
-//     return 'Not loaded';
-//   }
-// }
--- public/popup/popup.js
+++ public/popup/popup.js
@@ -1,1 +1,57 @@
-console.log('Popup working fine.');
+let labelEl = document.getElementById('lblExtVersion');
+
+window.addEventListener("load", (event) => {
+    // Get Current active tab execute code
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        // execute script to current tab
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: function () {
+                let detailsEl = document.getElementById('spyglass-ext-details');
+
+                if (detailsEl) {
+                    let resString = detailsEl.innerHTML;
+                    try {
+                        return {
+                            isSuccess: true,
+                            ...JSON.parse(resString)
+                        }
+                    } catch (e) {
+                        console.error(e);
+                        return {
+                            isSuccess: false,
+                            message: e.message
+                        };
+                    }
+                }
+
+                return {
+                    isSuccess: false,
+                    message: 'Detail element not found !!!'
+                };
+            },
+        }).then((response) => {
+            let data = response[0].result;
+            if (data.isSuccess) {
+                let { isExtJs, extJsVersion } = data;
+                let innerHtml = 'Site is not using ExtJS Framework';
+
+                if (isExtJs) innerHtml = `Site is using ExtJS Version ${extJsVersion}`
+                else console.error(data.message)
+
+                labelEl.innerHTML = innerHtml;
+
+                // Setting badge
+                chrome.action.setBadgeText({
+                    text: isExtJs ? 'On' : 'Off'
+                });
+
+                // Setting background color
+                chrome.action.setBadgeBackgroundColor({
+                    color: isExtJs ? 'green' : 'red'
+                });
+            }
+        });
+    });
+
+});
