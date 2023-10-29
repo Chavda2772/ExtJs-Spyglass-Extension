f85b6595001cc87a4e08bd554a37b415e14b66f0
Chavda Mahesh <chavdamahesh2772@gmail.com>
2023-10-29 14:15:53 +0530

    Removing Service worker from manifest json and code improvement

--- public/devtools/devtools.js
+++ public/devtools/devtools.js
@@ -1,15 +1,13 @@
 // Create the sidebar panel
-chrome.devtools.panels.elements.createSidebarPane(
-  'ExtJS Spyglass',
-  function (sidebar) {
+chrome.devtools.panels.elements.createSidebarPane('ExtJS Spyglass', function (sidebar) {
     // Handle the initialization of the sidebar panel
     sidebar.setPage('devtools/sidebarPanel.html');
     sidebar.setHeight('300px'); // Set the initial height of the sidebar panel
     sidebar.onShown.addListener(function (window) {
-      // Handle actions when the sidebar is shown
+        // Handle actions when the sidebar is shown
     });
     sidebar.onHidden.addListener(function () {
-      // Handle actions when the sidebar is hidden
+        // Handle actions when the sidebar is hidden
     });
-  }
+}
 );
